8b971bef81d7d93bd46e42cf324a1831bcadd5c6
Laursenx <77217858+laursenx@users.noreply.github.com>
2023-11-03 18:49:34 +0100

    Fixed average ping going crazy

--- src/server/api/routers/fivem.ts
+++ src/server/api/routers/fivem.ts
@@ -8,7 +8,7 @@ export const fivemRouter = createTRPCRouter({
 
     const averagePing =
       clients > 0
-        ? players.reduce((acc, cur) => acc + cur.ping, 0) / clients
+        ? (players.reduce((acc, cur) => acc + cur.ping, 0) / clients).toFixed(0)
         : 0;
 
     let restartPlan = process.env.RESTART_PLAN!;
